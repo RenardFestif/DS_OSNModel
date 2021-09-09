4def689b273520e32324a689cd3737f18029923a
Babouche <jub.saadi@gmail.com>
2021-09-09 18:15:06 +0200

    Post Feature & Test Added ESLINT for code format

--- src/helpers/IObserver.ts
+++ src/helpers/IObserver.ts
@@ -1,9 +1,10 @@
-import { Subject } from "./ISubject";
+import { Subject } from './ISubject';
 
 /**
  * The Observer interface declares the update method, used by subjects.
  */
 export interface Observer {
     // Receive update from subject.
+    // eslint-disable-next-line no-unused-vars
     update(subject: Subject): void;
 }
--- tests/UserTest.spec.ts
+++ tests/UserTest.spec.ts
@@ -1,35 +1,34 @@
+/* eslint-disable no-undef */
 import 'jasmine';
 
 import { DEFAULT_SCORE } from '../src/modules/Constant';
+import Content from '../src/modules/Content';
 import { Nature, User } from '../src/modules/User';
 
+let userA:User;
+let userB:User;
 
-var userA:User
-var userB:User 
+describe('UserImplementation', () => {
+  beforeEach(() => {
+    User.count = 0;
+    userA = new User();
+    userB = new User();
+  });
+  it('should initiate default values of 1 user', () => {
+    expect(userA.id).toEqual(1);
+    expect(userA.feed).toEqual([]);
+    expect(userA.follows).toEqual([]);
+    expect(userA.followers).toEqual([]);
+    expect(userA.score).toEqual(DEFAULT_SCORE);
+    expect(userA.nature).toEqual(Nature.MALICIOUS || Nature.AVERAGE || Nature.TRUTHFULL);
+  });
 
+  it('should increment the id when a new user is instanciated', () => {
+    expect(userB.id).toEqual(2);
+    expect(User.count).toEqual(2);
+  });
 
-describe("UserImplementation",function() { 
-    beforeAll(function(){
-        User.count = 0;
-        userA = new User(); 
-        userB = new User();
-    })
-    it("should initiate default values of 1 user", function() {
-        
-        expect(userA.id).toEqual(1);
-        expect(userA.feed).toEqual([]);
-        expect(userA.follows).toEqual([]);
-        expect(userA.followers).toEqual([]);
-        expect(userA.score).toEqual(DEFAULT_SCORE);
-        expect(userA.nature).toEqual(Nature.MALICIOUS || Nature.AVERAGE || Nature.TRUTHFULL);
-           
-    });
-
-    it("should increment the id when a new user is instanciated", function(){
-        expect(userB.id).toEqual(2);
-        expect(User.count).toEqual(2);
-    })
-
-   
-
+  it('should write a new post', () => {
+    expect(userA.writeContent()).toBeInstanceOf(Content);
+  });
 });
